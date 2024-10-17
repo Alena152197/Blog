f2284d989227884acfeaec0c84bbeda5b56b4d35
Furajkova Alena <Furajlova@mail.ru>
2024-10-17 21:09:28 +0300

    dom

--- client/app.vue
+++ client/app.vue
@@ -2,7 +2,9 @@
   <NuxtLoadingIndicator throttle="0" />
   <Navbar />
   <NuxtPage />
+  <Up />
   <Footer />
+
 </template>
 
 <script setup>
@@ -11,6 +13,7 @@ const api = await $fetch(`${base}/api/config?populate=*`)
 const config = api.data
 
 useHead({
+  
   title: config.title,
   meta: [
     { name: 'description', content: config.desc },
@@ -18,28 +21,9 @@ useHead({
   ],
   link: [
     { rel: 'icon', type: 'image/x-icon', href: base + config.favicon.url }
-  ]
+  ],
+  bodyAttrs: {
+    class: 'container mx-auto'
+  },
 })
-
 </script>
-
-<style scoped>
-html,
-body,
-#__nuxt {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
-
-body {
-  margin: 0;
-  background: linear-gradient(180deg, #0000 0%, #0000 100%), url("http://localhost:1337/uploads/orig_35d41e9945.webp");
-  background-blend-mode: darken;
-}
-
-main {
-  flex: 1 1 auto;
-}
-</style>
--- client/pages/blog.vue
+++ client/pages/blog.vue
@@ -1,7 +1,32 @@
 <template>
     <h1>Блог</h1>
     <main>
-        <article v-for="(post, index) in posts" :key="post.id">
+        <div class="articles">
+            <article v-for="(post, index) in posts" :key="post.id"
+                class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" :src="base_url + post.img[0].url" :alt="post.img[0].alternativeText" />
+                </a>
+                <div class="p-5">
+                    <NuxtLink :to="'/post/' + post.documentId">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ post.title }}</h5>
+                    </NuxtLink>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ post.desc }}</p>
+                    <NuxtLink :to="'/post/' + post.documentId">
+                        <p class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Подробнее
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </NuxtLink>
+                </div>
+            </article>
+        </div>
+
+
+        <!-- <article v-for="(post, index) in posts" :key="post.id">
             <h3>{{ post.title }}</h3>
             <div class="link">
                 <img :src="base_url + post.img[0].url" :alt=post.img[0].alternativeText>
@@ -12,10 +37,10 @@
             <ul class="tag">
                 <li v-for="(category, index) in post.categories" :key="category.id">
                     <NuxtLink :style="'background:' + post.categories[index].bg"
-                        :to="'/category' + post.categories[0].documentId">{{ category.title }}</NuxtLink>
+                        :to="'/category/' + post.categories[0].documentId">{{ category.title }}</NuxtLink>
                 </li>
             </ul>
-        </article>
+        </article> -->
     </main>
 </template>
 
@@ -26,86 +51,3 @@ const posts = api.data
 
 const base_url = "http://localhost:1337"
 </script>
-
-
-<style scoped>
-main {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    /* grid-template-columns: repeat(auto-fit, minmax (250px, 1fr));
-    grid-auto-rows: minmax(100px, auto);
-    grid-template-rows: repeat(4, 1fr); */
-    gap: 40px;
-    padding: 40px;
-}
-
-@media screen and (max-width: 1024px) {
-    .posts {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        gap: 25px;
-    }
-}
-
-@media screen and (max-width: 768px) {
-    .posts {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 25px;
-    }
-}
-
-@media screen and (max-width: 640px) {
-    .posts {
-        display: grid;
-        grid-template-columns: repeat(1, 1fr);
-        gap: 25px;
-    }
-}
-
-article {
-    width: 300px;
-    background-color: #f5f5f5;
-    padding: 10px;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    border: 5px solid wheat;
-    border-radius: 15px;
-}
-
-article img {
-    width: 100%;
-    height: 150px;
-    object-fit: cover;
-    border-radius: 10px;
-}
-
-article p {
-    height: 80px;
-}
-
-article a {
-    display: block;
-    text-decoration: none;
-    font-weight: 500;
-    border: 2px inset red;
-    border-radius: 5px;
-    padding: 5px;
-    background-color: wheat;
-    width: 100px;
-    text-align: center;
-}
-
-article a:hover {
-    padding: 7px;
-    color: red;
-}
-
-
-.link {
-    position: relative;
-    width: 300px;
-}
-</style>
